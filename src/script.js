2993650cf3db7deb56055fde1aa7355e4f171a7e
Manu <manuprudente04@gmail.com>
2024-05-26 20:49:15 +0200

    Added base for the sliders

--- src/script.js
+++ src/script.js
@@ -1,22 +1,16 @@
-// Creating the HTML slider element
-const sliderObject = document.createElement('div');
-sliderObject.className = 'slider-object';
-const sliderHeader = document.createElement('h2');
-sliderHeader.className = 'slider-header';
-const headerText = document.createTextNode('Test')
-sliderHeader.append(headerText);
-sliderObject.append(sliderHeader);
+const sliderSource = document.getElementById('slider-template').innerHTML;
 
-const container = document.getElementsByClassName('container')[0];
+const sliderTemplate = Handlebars.compile(sliderSource);
 
-// calling an 'appendChild' multiple times doesn't work, as the same node is being re-added.
-// we need to create another element each time. Hopefully, cloneNode does that for us
-
-const clamp = (min, num, max) => Math.min(Math.max(num, min), max)
-
-let numOfSliders = clamp(1, Math.floor(window.innerWidth / 100), 8) ;
-console.log(numOfSliders);
-
-for (let i = 0; i < numOfSliders; i++) {
-    container.append(sliderObject.cloneNode(true))
+const data = {
+    headers: ['Master', 'Drums', 'Synth', 'Guitar', 'Bass', 'Piano', 'Strings all day heheheha', 'Drone'],
+    test: 'test',
 }
+
+
+
+const compiledSlider = sliderTemplate(data);
+
+const targetContainer = document.getElementsByClassName('container')[0];
+
+targetContainer.innerHTML = compiledSlider;
